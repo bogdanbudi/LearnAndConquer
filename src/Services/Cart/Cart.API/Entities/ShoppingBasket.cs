2dab253ec0a82daf1987999381a68a0c90e4e1ed
Bogdan <bogdan.florin27@yahoo.com>
2022-01-08 18:48:54 +0200

    Create Entities, connection to Redis and configuration

---
+++ src/Services/Cart/Cart.API/Entities/ShoppingBasket.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Cart.API.Entities
+{
+    public class ShoppingBasket
+    {
+        public string UserName { get; set; }
+
+        public List<ShoppingBasketItem> ShoppingItems { get; set; } = new List<ShoppingBasketItem>();
+
+        public ShoppingBasket()
+        {
+
+        }
+
+        public ShoppingBasket(string userName)
+        {
+            this.UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach(var item in ShoppingItems)
+                {
+                    totalprice += item.Price;
+                }
+                return totalprice;
+            }
+        }
+
+    }
+}
---
+++ src/Services/Cart/Cart.API/Entities/ShoppingBasketItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Cart.API.Entities
+{
+    public class ShoppingBasketItem
+    {
+
+        public string IdCourse { get; set; }
+        public string NameCourse { get; set; }
+        public decimal Price { get; set; }
+
+
+
+    }
+}
