e9e27a148c40f37c65918ac9da04b01775ffe686
Bogdan <bogdan.florin27@yahoo.com>
2022-01-08 20:21:55 +0200

    Add Cart Repository Pattern

---
+++ src/Services/Cart/Cart.API/Repository/CartRepository.cs
@@ -0,0 +1,42 @@
+﻿using Cart.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cart.API.Repository
+{
+    public class CartRepository: ICartRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public CartRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+
+        public async Task<ShoppingBasket> GetCart(string userName)
+        {
+            var cart = await _redisCache.GetStringAsync(userName);
+
+            if (String.IsNullOrEmpty(cart))
+                return null;
+
+            return JsonConvert.DeserializeObject<ShoppingBasket>(cart);
+        }
+
+        public async Task<ShoppingBasket> UpdateCart(ShoppingBasket cart)
+        {
+            await _redisCache.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+
+            return await GetCart(cart.UserName);
+        }
+
+        public async Task DeleteCart(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+    }
+}
---
+++ src/Services/Cart/Cart.API/Repository/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using Cart.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cart.API.Repository
+{
+    public interface ICartRepository
+    {
+        Task<ShoppingBasket> GetCart(string userName);
+        Task<ShoppingBasket> UpdateCart(ShoppingBasket cart);
+        Task DeleteCart(string userName);
+    }
+}
