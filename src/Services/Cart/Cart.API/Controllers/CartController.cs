aa4b33c78e8c56a62d4ff0c95e74f938b0ded593
Bogdan <bogdan.florin27@yahoo.com>
2022-01-08 20:46:36 +0200

    Add Controller

---
+++ src/Services/Cart/Cart.API/Controllers/CartController.cs
@@ -0,0 +1,47 @@
+﻿using Cart.API.Entities;
+using Cart.API.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Cart.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepository _repository;
+
+
+        public CartController(ICartRepository repository) 
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("{userName}", Name = "GetCart")]
+        [ProducesResponseType(typeof(ShoppingBasket), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingBasket>> GetCart(string userName)
+        {
+            var cart = await _repository.GetCart(userName);
+            return Ok(cart ?? new ShoppingBasket(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingBasket), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingBasket>> UpdateCart([FromBody] ShoppingBasket basket)
+        { 
+            return Ok(await _repository.UpdateCart(basket));
+        }
+
+        [HttpDelete("{userName}", Name = "DeleteCart")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteCart(string userName)
+        {
+            await _repository.DeleteCart(userName);
+            return Ok();
+        }
+    }
+}
