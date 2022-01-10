2d5ad20454d05b6283f1528e2a3c3127bc8b577d
Bogdan <bogdan.florin27@yahoo.com>
2022-01-10 22:08:03 +0200

    Add Ordering Api with DDD, CQRS, CLEAN CODE  // not finished

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandValidator: AbstractValidator<CheckoutOrderCommand>
+    {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(p => p.UserName)
+                .NotNull()
+                .NotEmpty().WithMessage("{Username} is required.")
+                .MaximumLength(30).WithMessage("{UserName} must not be exceed 30 characters.");
+
+            RuleFor(p => p.TotalPrice)
+               .NotEmpty().WithMessage("{TotalPrice} is required.")
+               .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+
+            RuleFor(p => p.EmailAddress)
+               .NotNull()
+               .NotEmpty().WithMessage("{EmailAddress} is required.")
+               .MinimumLength(10).WithMessage("{EmailAddress}  should be greater than 10 characters.");
+
+        }
+    }
+}
