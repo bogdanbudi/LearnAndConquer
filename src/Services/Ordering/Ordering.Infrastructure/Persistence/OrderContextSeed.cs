11565337741607f88d76adc3f265de5aa1504ac6
Bogdan <bogdan.florin27@yahoo.com>
2022-01-11 01:21:24 +0200

    ////////////////////////////////////  INFRASTRUCTURE   //////////////////////////////// ////////////////////////////   Add Entity Frame and create Persistence  /////////////////////////////////

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "badita1", FirstName = "Badita", LastName = "Bogdan", EmailAddress = "badita.bogdan27@gmail.com", AddressLine = "Bucuresti", Country = "Romania", TotalPrice = 350 }
+            };
+        }
+    }
+}
