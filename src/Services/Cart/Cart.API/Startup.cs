f1f6ed9ef2ac4cd04765c4c555818a7ce9b99921
Bogdan <bogdan.florin27@yahoo.com>
2022-01-11 05:00:57 +0200

    Add Services and Package Nuget for MassTranzit and RabbitMQ

--- src/Services/Cart/Cart.API/Startup.cs
+++ src/Services/Cart/Cart.API/Startup.cs
@@ -1,6 +1,7 @@
 using Cart.API.GrpcServices;
 using Cart.API.Repository;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -35,11 +36,21 @@ namespace Cart.API
             }
             );
 
+            //General Configuration
             services.AddScoped<ICartRepository, CartRepository>();
 
+            //Grpc Configuration
             services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
 
             services.AddScoped<DisocuntGrpcService>();
+
+            // MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config => {
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                });
+            });
+            services.AddMassTransitHostedService();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
